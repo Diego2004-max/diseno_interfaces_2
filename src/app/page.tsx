f0a6d8c6237e232386e269c0c58b17895352bd5b
Diego Alejandro Mallama Yandun <diego.mallamay@campusucc.edu.co>
2025-08-26 09:22:28 -0500

    Actualizacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-8">
-  {/* card */}
     <div className="bg-white rounded-2xl shadow-lg flex w-full max-w-4xl overflow-visible p-6">
-    
-    {/* columna izquierda (cuadro gris) */}
+
     <div className="relative w-1/2 min-h-[520px] bg-gradient-to-br from-gray-50 to-gray-300
                     rounded-3xl shadow-inner border border-gray-300/50 overflow-visible">
       <img 
@@ -15,16 +13,13 @@ export default function HomePage() {
           />
         </div>
 
-        {/* Columna derecha con formulario */}
         <div className="w-1/2 p-12 flex flex-col justify-between">
-          
-          {/* Header arriba a la derecha */}
+
           <div className="text-right text-sm mb-6">
             <span className="text-gray-400">Don't have an account?</span>
-            <a href="#" className="ml-2 text-black font-semibold">Sign up</a>
+            <a href="" className="ml-2 text-black font-semibold">Sign up</a>
           </div>
 
-          {/* Contenido del login */}
           <div>
             <h2 className="text-2xl font-bold mb-4 text-black">Sign in</h2>
             
@@ -43,9 +38,7 @@ export default function HomePage() {
             <p className="text-gray-500 text-center mb-4 font-semibold text-sm">
               Or continue with email address
             </p>
-          </div>
-
-          {/* Inputs y botón */}
+          </div> 
           <div className="space-y-3">
             <button className="w-full py-2 border rounded-lg flex items-center justify-start space-x-3 bg-gray-100 hover:bg-white px-3">
               <img src="/gmail.webp" alt="Email" className="w-8 h-8 object-cover"/>
